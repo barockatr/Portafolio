bd6746652312597fce9d983ecffebf56d0648b48
barockatr <antonioatristain06@gmail.com>
2024-02-13 15:02:34 -0800

    html.

--- scripts/clean.js
+++ scripts/clean.js
@@ -3,5 +3,12 @@ const upath = require('upath');
 
 const destPath = upath.resolve(upath.dirname(__filename), '../dist');
 
-sh.rm('-rf', `${destPath}/*`)
+// Define una función para excluir archivos CSS
+function filterOutCSS(file) {
+  return !file.endsWith('.css');
+}
 
+// Elimina todos los archivos excepto los archivos CSS
+sh.ls(destPath).filter(filterOutCSS).forEach(file => {
+  sh.rm('-rf', upath.join(destPath, file));
+});
